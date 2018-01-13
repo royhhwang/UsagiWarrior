c05f7ac1bcaef80c59f98e1f5d1af36c940e0197
Jay Yang <hjyang1@uci.edu>
2018-01-13 15:21:25 -0800

    Weapon Class added

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -8,7 +8,7 @@ public class PlayerHealth : MonoBehaviour {
     public int startingHealth = 100;
     public int currentHealth;
     public Slider healthSlider;
-    public Image damageImage;
+    public Image DamageImage;
     public AudioClip deathClip;
     public float flashSpeed = 5f;
     public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
@@ -36,12 +36,12 @@ public class PlayerHealth : MonoBehaviour {
 	void Update () {
         if (damaged)
         {
-            damageImage.color = flashColor;
+            DamageImage.color = flashColor;
 
         }
         else
         {
-            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+            DamageImage.color = Color.Lerp(DamageImage.color, Color.clear, flashSpeed * Time.deltaTime);
         }
         damaged = false;
 	}
@@ -53,7 +53,7 @@ public class PlayerHealth : MonoBehaviour {
         currentHealth -= amount;
 
         healthSlider.value = currentHealth;
-        playerAudio.Play();
+        //playerAudio.Play();
         if(currentHealth <= 0 && !isDead)
         {
             Dead();
@@ -64,11 +64,13 @@ public class PlayerHealth : MonoBehaviour {
     {
         isDead = true;
 
-        anim.SetTrigger("Die");
+        //anim.SetTrigger("Die");
 
-        playerAudio.clip = deathClip;
-        playerAudio.Play();
+        //playerAudio.clip = deathClip;
+        //playerAudio.Play();
 
         playerMovement.enabled = false;
+
+        Debug.Log("You died! Movement disabled");
     }
 }
---
+++ Assets/Scripts/Player/Weapon/WeaponSwing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwing : MonoBehaviour {
+
+    private float swingSpeed;
+    private float swingReloadTime;
+    private float swingAngle;
+    private float normalAngle;
+    private GameObject Sword;
+
+	// Use this for initialization
+	void Start () {
+        swingSpeed = 5f;
+        swingReloadTime = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1"))
+        {
+            Vector3 v = Sword.transform.eulerAngles;
+            transform.rotation.eulerAngles.z = Mathf.Lerp(Sword.transform.rotation.eulerAngles.z, swingAngle, Time.deltaTime * swingSpeed);
+        }
+        else
+        {
+            Sword.transform.rotation.eulerAngles.z = Mathf.Lerp(Sword.transform.rotation.eulerAngles.z, normalAngle, Time.deltaTime * swingSpeed);
+        }
+    }
+}
