d8e607499587fa1a363e71a37eb9d1ba4658eb18
Jay Yang <hjyang1@uci.edu>
2018-01-14 15:49:14 -0800

    Weapon Attack and Weapon Idle Animations Complete

---
+++ Assets/Scripts/Player/Weapon/WeaponAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAttack : MonoBehaviour {
+    private Animator animator;
+    
+
+	// Use this for initialization
+	void Start () 
+    {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown("Fire1"))
+        {
+            BasicAttack();    
+        }
+	}
+
+    public void BasicAttack()
+    {
+        animator.SetTrigger("BaseAttack");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if(other.tag == "Enemy")
+        {
+           // other.TakeDamage();
+        }
+    }
+}
