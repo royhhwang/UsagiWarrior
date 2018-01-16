c8e242ba950bfa9991bc843f1855fddcf903f04d
Jay Yang <hjyang1@uci.edu>
2018-01-15 18:01:23 -0800

    Basic Enemy Take Damage and Destroy Character Implemented

---
+++ Assets/Scripts/Enemy/BasicEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour, IEnemy
+{
+    public float startingHealth, power;
+    private float currentHealth;
+
+    void Start()
+    {
+        currentHealth = startingHealth;
+    }
+    // Use this for initialization
+    public void PerformAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Dead();
+        }
+    }
+
+    void Dead()
+    {
+        Destroy(gameObject);
+    }
+}
