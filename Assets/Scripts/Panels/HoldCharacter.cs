9615e0a4b79bffab8854a13c5a095c2ad871b9d9
Jay Yang <hjyang1@uci.edu>
2018-01-18 21:23:01 -0800

    Working lateral and vertical platforms + sticky platform-player implemented

---
+++ Assets/Scripts/Panels/HoldCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldCharacter : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            collider.transform.parent = gameObject.transform;
+        }
+        
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            collider.transform.parent = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Panels/MovingPlatforms.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatforms : MonoBehaviour {
+
+    public Transform movingPlatform;
+    public Transform position1;
+    public Transform position2;
+
+    public Vector3 newPosition;
+    public string currentState;
+    public float smooth;
+    public float resetTime;
+
+	// Use this for initialization
+	void Start () {
+        ChangeTarget();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        movingPlatform.position = Vector3.Lerp(movingPlatform.position, newPosition, Time.deltaTime * smooth);
+	}
+
+    void ChangeTarget()
+    {
+        if(currentState == "Move to position 1")
+        {
+            currentState = "Move to position 2";
+            newPosition = position2.position;
+        }
+        else
+        {
+            currentState = "Move to position 1";
+            newPosition = position1.position;
+        }
+        
+        Invoke("ChangeTarget", resetTime);
+    }
+}
