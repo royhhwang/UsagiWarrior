c023f32950140565ae295da14be63bce58aabb28
Jay Yang <hjyang1@uci.edu>
2018-01-22 15:44:26 -0800

    finished adding new model and animations

---
+++ Assets/Scripts/Player/LandedAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandedAnimation : MonoBehaviour {
+
+    public GameObject player;
+    PlayerMovement playerMovement;
+
+    
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Platform")
+        {
+            playerMovement.playerGrounded = false;
+        }
+    }
+    
+       
+    
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,24 +11,30 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 18f;
     public float gravity = 40f;
     private Vector3 moveDir;
+    public bool playerGrounded;
+    public bool playerFalling;
 
-    public GameObject leftFoot;
-    public GameObject rightFoot;
+    public GameObject body;
+
+    Animator animator;
+    
 
     // Use this for initialization
     void Start()
     {
+        animator = GetComponent<Animator>();
         moveDir = Vector3.zero;
     }
 
+   
     // Update is called once per frame
     void Update()
     {
-        CharacterController controller = gameObject.GetComponent<CharacterController>();
+        CharacterController controller = body.GetComponentInParent<CharacterController>();
+        
         
         if (controller.isGrounded)
         {
-
             moveDir = new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed, 0f, Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed);
             moveDir = transform.TransformDirection(moveDir);
             moveDir *= moveSpeed;
@@ -36,37 +42,25 @@ public class PlayerMovement : MonoBehaviour
             if (Input.GetButtonDown("Jump"))
             {
                 moveDir.y = jumpForce;
-
-                CallAnimation(rightFoot, "Jump");
-                CallAnimation(leftFoot, "Jump");
+                animator.SetTrigger("Jump");
             }
-
             
-        }
-
-        if (controller.isGrounded)
-        {
-            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            if (moveDir == new Vector3 (0, 0, 0))
             {
-                CallAnimation(leftFoot, "Walk");
-                CallAnimation(rightFoot, "Walk");
+                animator.SetTrigger("Idle");
             }
-            else
-            {
-                CallAnimation(leftFoot, "Idle");
-                CallAnimation(rightFoot, "Idle");
+            else if (moveDir.y == 0)
+            { 
+                animator.SetTrigger("Walk");
             }
         }
-
-
         moveDir.y -= gravity * Time.deltaTime;
 
         controller.Move(moveDir * Time.deltaTime);
-
+        
+        
     }
     
-    void CallAnimation (GameObject obj, string type)
-    {
-        obj.GetComponent<Animator>().SetTrigger(type);
-    }
 }
+
+
